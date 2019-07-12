97235ddfac28fbe878a7121e3a03d5b021ef5abb
shubham0909 <shubhamtile@gmail.com>
2019-07-12 11:48:47 +0530

    update

--- src/app/order-menu/order-menu.component.ts
+++ src/app/order-menu/order-menu.component.ts
@@ -19,6 +19,45 @@ export class OrderMenuComponent implements OnInit {
 
   ngOnInit() {
     this.TodayDate = new Date();
+    this.QunitityJs();
+
+  }
+
+  QunitityJs(){
+    $(document).ready(function(){
+      $('.count-a').prop('disabled', true);
+       $(document).on('click','.plus-a',function(){
+      $('.count-a').val(parseInt($('.count-a').val()) + 1 );
+      });
+        $(document).on('click','.minus-a',function(){
+        $('.count-a').val(parseInt($('.count-a').val()) - 1 );
+          if ($('.count-a').val() == 0) {
+          $('.count-a').val(1);
+        }
+          });
+
+          $('.count-b').prop('disabled', true);
+          $(document).on('click','.plus-b',function(){
+         $('.count-b').val(parseInt($('.count-b').val()) + 1 );
+         });
+           $(document).on('click','.minus-b',function(){
+           $('.count-b').val(parseInt($('.count-b').val()) - 1 );
+             if ($('.count-b').val() == 0) {
+             $('.count-b').val(1);
+           }
+             });
+
+             $('.count-c').prop('disabled', true);
+             $(document).on('click','.plus-c',function(){
+            $('.count-c').val(parseInt($('.count-c').val()) + 1 );
+            });
+              $(document).on('click','.minus-c',function(){
+              $('.count-c').val(parseInt($('.count-c').val()) - 1 );
+                if ($('.count-c').val() == 0) {
+                $('.count-c').val(1);
+              }
+                });
+   });
   }
 
 }
